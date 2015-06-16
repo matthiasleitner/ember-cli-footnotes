9bf3bd9f4afaf4d9ae7f7be8cacd95dc3c54c848
Matthias Leitner <matthias.leitner@runtastic.com>
2015-06-16 10:31:26 +0200

    load styles from file

--- index.js
+++ index.js
@@ -4,6 +4,8 @@
 require('shelljs/global');
 var getRepoInfo = require('git-repo-info');
 var info = getRepoInfo();
+var path = require('path');
+var readFileSync = require('fs').readFileSync;
 var userInfo, footnotesStyles, footnotesInfo;
 
 userInfo = function() {
@@ -13,26 +15,15 @@ userInfo = function() {
 }
 
 footnotesStyles = function() {
-  return '<style> \
-            body { \
-              position:relative; \
-            } \
-            .ember-cli-footnotes {\
-              position: absolute; \
-              bottom: 0px; \
-              right: 0px; \
-              padding: 5px; \
-              background: rgba(255, 255, 255, 0.9); \
-              z-index: 1000; \
-            } \
-          </style>';
+  var fileName = path.join(__dirname, './addon/styles/footnotes.css');
+  return '<style>' + readFileSync(fileName) + '</style>';
 }
 
 footnotesInfo = function() {
   return '<div class="ember-cli-footnotes"> \
               <p> Branch: ' + info.branch + '</p> \
               <p> SHA: ' + info.sha + '</p> \
-              <p> Deployed by: ' + userInfo() + '</p> \
+              <p> Deployed by: ' + userInfo() + ' at ' + new Date().toString() + '</p> \
             </div>'
 }
 
